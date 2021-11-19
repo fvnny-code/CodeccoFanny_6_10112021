9cc17cf8e2be34944bac16f3536854019c7b2cca
fvnny-code <f.codecco.grando@gmail.com>
2021-11-19 17:18:04 +0100

    configuration du middleware d'authentification

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,8 @@ const userRoutes = require('./routes/user');
 mongoose
   .connect(
     "mongodb+srv://fvnny-code:Pipite@cluster1piiquante.aojie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+    { useNewUrlParser: true, 
+      useUnifiedTopology: true })
   .then(() => console.log("Connexion à la base de données réussie !"))
   .catch(() => console.log("La connexion à la base de données a échoué !"));
 
@@ -17,18 +17,11 @@ const app = express();
 
 //  Gestion du Cross Origins Ressource Sharing
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH,OPTIONS"
-  );
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
-
 app.use(bodyParser.json());
 
 app.use('/api/sauces', SaucesRoutes);
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -3,16 +3,17 @@ const router = express.Router();
 const Sauce = require("../models/Sauce");
 
 const sauceController = require("../controllers/sauces");
+const auth = require('../middelware/auth')
 
 //création d'une sauce
-router.post("/", sauceController.createSauce);
+router.post("/", auth, sauceController.createSauce);
 //modification d'une sauce
-router.put("/:id", sauceController.modifySauce);
+router.put("/:id", auth, sauceController.modifySauce);
 // Suppression d'un sauce
-router.delete("/:id", sauceController.deleteSauce);
+router.delete("/:id", auth, sauceController.deleteSauce);
 // récupération d'une sauce spécifique
-router.get("/:id", sauceController.getOneSauce);
+router.get("/:id", auth, sauceController.getOneSauce);
 // récupération de la liste des sauces
-router.get("/", sauceController.getAllSauces);
+router.get("/", auth, sauceController.getAllSauces);
 
 module.exports = router;
