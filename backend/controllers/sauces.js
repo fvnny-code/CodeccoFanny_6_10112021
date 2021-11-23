d04d132c077aeb557f1cec5112e5ff1418b243e1
fvnny-code <f.codecco.grando@gmail.com>
2021-11-23 12:01:20 +0100

    modification des routes PUT et DELETE

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,4 +1,7 @@
+// importe le schéma de la sauce
 const Sauce = require("../models/Sauce");
+// package File System pour gérer l'import et export de fichiers
+const fs = require('fs');
 
 // création d'une sauce
 exports.createSauce = (req, res, next) => {
@@ -13,6 +16,7 @@ exports.createSauce = (req, res, next) => {
     .then(() => res.status(201).json({ message: "sauce enregistrée." }))
     .catch((error) => res.status(400).json({ error }));
 };
+
 // modification d'une sauce
 exports.modifySauce = (req, res, next) => {
   const sauceObject = req.file ?
@@ -24,19 +28,29 @@ exports.modifySauce = (req, res, next) => {
     .then(() => res.status(200).json({ message: "Objet modifié! " }))
     .catch((error) => res.status(400).json({ error }));
 };
+
 // suppression d'une sauce
 exports.deleteSauce = (req, res, next) => {
-  Sauce.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {
+      const filename = sauce.imageUrl.split('/images/') [1];
+      fs.unlink(`images/${filename}`, () => {
+        Sauce.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: "Objet supprimé !" }))
     .catch((error) => res.status(400).json({ error }));
+      });
+    })
+   .catch(error => res.status(500).json({error})); 
 };
+
 //récupération d'une sauce
 exports.getOneSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((thing) => res.status(200).json(sauce))
     .catch((error) => res.status(404).json({ error }));
 };
-// récupération de la liste des sauces
+
+// récupération de la liste de toutes les sauces
 exports.getAllSauces = (req, res, next) => {
   Sauce.find()
     .then((sauces) => res.status(200).json(sauces))
