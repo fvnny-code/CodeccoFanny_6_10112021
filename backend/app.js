0bec49dfad5f7821ac0e69c6da198ee69b901360
fvnny-code <f.codecco.grando@gmail.com>
2021-11-23 15:37:08 +0100

    fix des erreurs CORS

--- backend/app.js
+++ backend/app.js
@@ -18,18 +18,14 @@ mongoose
 const app = express();
 
 //  Gestion du Cross Origins Ressource Sharing
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//   next();
+// });
 
-// app.use(cors({
-//   origin: '*',
-//   methods : ['GET','POST','DELETE','UPDATE','PUT','PATCH', 'OPTIONS']
-// }));
-
+app.use(cors());
 
 
 app.use(bodyParser.json());
