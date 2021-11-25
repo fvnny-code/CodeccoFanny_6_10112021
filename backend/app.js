eab3a5ef5f840fc8e0de0e734b954310d0785728
fvnny-code <f.codecco.grando@gmail.com>
2021-11-25 11:11:34 +0100

    reconfiguration de NodeJs + Angular dans les versions plsu anciennes Installation de tous les packages nécessaires

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,38 @@
+//import d'Expess (framework Node JS)
 const express = require("express");
-const bodyParser = require("body-parser");
+//Import du pack Mongoose : gestion de la base de données
 const mongoose = require("mongoose");
-const cors = require('cors');
+//Import de bodyParser : extraction des objets JSON depuis une requête POST
+const bodyParser = require("body-parser");
+//Gestion des paths des fichiers et dossiers
 const path = require('path');
+//import du pack Dotenv = masquage des informations de connexion.
+require('dotenv').config();
 
+// import du pack CORS pour la gestion des Cross Origins Ressource Sharing
+const cors = require('cors');
+
+// création de l'application Express
+const app = express();
+
+//import des routes vers les sauces
 const SaucesRoutes = require("./routes/sauces");
+//import des routes utilisateurs
 const userRoutes = require('./routes/user');
 
-mongoose.connect(
-    "mongodb+srv://fvnny-code:Pipite@cluster1piiquante.aojie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    { useNewUrlParser: true, 
-      useUnifiedTopology: true })
-  .then(() => console.log("Connexion à la base de données réussie !"))
-  .catch(() => console.log("La connexion à la base de données a échoué !"));
 
-const app = express();
+mongoose.connect(
+    // process.env.DB_URI,
+    'mongodb+srv://fvnny-code:Pipite@cluster1piiquante.aojie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    { 
+      // useCreateIndex :true,
+      useNewUrlParser: true, 
+      useUnifiedTopology: true 
+    })
+  .then(() => console.log("Connexion à la base de données réussie !"))
+  .catch((error) => console.log("La connexion à la base de données a échoué !"));
+
+
 
 //  Gestion du Cross Origins Ressource Sharing
 // app.use((req, res, next) => {
@@ -26,7 +44,7 @@ const app = express();
 
 app.use(cors());
 
-
+//Transforme les données de la requête POST method en JSON
 app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -18,12 +18,25 @@ exports.createSauce = (req, res, next) => {
 
 // modification d'une sauce
 exports.modifySauce = (req, res, next) => {
+
+  
   const sauceObject = req.file ?
   {
     ...JSON.parse(req.body.sauce),
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+
   } : {...req.body};
-  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+  if (req.file){
+    Sauce.findOne({ _id: req.params.id })
+    .then(sauce =>{
+      const filename = sauce.imageUrl.split('/images/') [1];
+      console.log(filename);
+      fs.unlink(`images/${filename}`, () => {
+        console.log(`images/${filename} a été supprimée`);
+      })
+    })
+  }
+  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
     .then(() => res.status(200).json({ message: "Objet modifié! " }))
     .catch((error) => res.status(400).json({ error }));
 };
