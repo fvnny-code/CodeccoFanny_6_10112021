2cb2586ae0be346e32656a3284b66d92038764d4
fvnny-code <f.codecco.grando@gmail.com>
2022-02-01 09:45:52 +0100

    modif dans gestion des cors

--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,39 @@
 //import du pack Dotenv = masquage des informations de connexion.
-require('dotenv').config();
+require("dotenv").config();
 
 //import d'Expess (framework Node JS)
 const express = require("express");
 //Import du pack Mongoose : gestion de la base de données
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 //Import de bodyParser : extraction des objets JSON depuis une requête POST
 const bodyParser = require("body-parser");
 
 // Import helmet : sécurise les requêtes HTTP, et les failles XSS(cross-site scripting), prévient du détournement de clics (clickjacking), protège contre le reniflement de TYPE MIME(snifing).
-const helmet =require("helmet");
+const helmet = require("helmet");
 // Import nocache : desactive la mise en cache du navigateur
 const nocache = require("nocache");
+const cors = require("cors");
 
 //import des routes vers les sauces
 const SaucesRoutes = require("./routes/sauces");
 //import des routes utilisateurs
-const userRoutes = require('./routes/user');
+const userRoutes = require("./routes/user");
 //Gestion des paths des fichiers et dossiers
-const path = require('path');
+const path = require("path");
 
 const app = express();
 
 // connexion à la base de données
 const DB_URI = process.env.DB_URI;
-mongoose.connect(
-    `${DB_URI}`, 
-    { useNewUrlParser: true, 
-      useUnifiedTopology: true }
-    )
+mongoose
+  .connect(`${DB_URI}`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connexion à la base de données réussie !"))
-  .catch((error) => console.log("La connexion à la base de données a échoué !"));
+  .catch((error) =>
+    console.log("La connexion à la base de données a échoué !")
+  );
 
 //  Gestion du Cross Origins Ressource Sharing
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+app.use(cors());
 //Transforme les données de la requête POST en JSON
 app.use(bodyParser.json());
 
@@ -49,10 +44,9 @@ app.use(helmet());
 app.use(nocache());
 
 // ROUTES
-app.use('/api/sauces', SaucesRoutes);
-app.use('/api/auth', userRoutes);
+app.use("/api/sauces", SaucesRoutes);
+app.use("/api/auth", userRoutes);
 // Gestion du routage des images
-app.use('/images', express.static(path.join(__dirname, 'images')));
-
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
